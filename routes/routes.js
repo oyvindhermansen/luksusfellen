236a48d161acc3065f67b066932dcb61c7fc7919
oyvindhermansen <oyvind@ludensreklame.no>
2016-04-17 13:18:14 +0200

    you can now post income to /board route

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,17 @@
-'use strict';
+'use strict'
 
 export default function(app) {
   app.get('/', (req, res) => {
-    res.render('index');
-  });
+    res.render('index')
+  })
+
+  app.get('/board', (req, res) => {
+    res.render('board')
+  })
+
+  app.post('/board', (req, res) => {
+    const income = req.body.income
+    res.render('board', {income: income})
+    console.log(income)
+  })
 }
