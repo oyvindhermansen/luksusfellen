d1d3958c2c83ae7aec66190a42ec00bdb766bdad
oyvindhermansen <oyvind@ludensreklame.no>
2016-04-18 00:02:47 +0200

    :beer:

--- resources/js/modules/drop.js
+++ resources/js/modules/drop.js
@@ -3,18 +3,30 @@ import $ from 'jquery'
 import { droppable } from 'jquery-ui/droppable'
 import { draggable } from './drag'
 
-
 function dropit() {
   const $dropArea = $('.board__main')
   $dropArea.droppable({
     hoverClass: 'active-money-hover',
     accept: '.bill',
     drop: function(event, ui) {
-      const $clone = ui.helper.clone()
-      const $this = $(this)
-      $this.append($clone)
+      const $income = $('.board__income-current').text()
+      let incomeValue = parseInt($income)
+      if (incomeValue > 0) {
+        const $clone = ui.helper.clone()
+        const $this = $(this)
+        const $dataBill = $clone.attr('data-bill')
+        let billValue = parseInt($dataBill)
 
-
+        if (incomeValue - billValue < 0) {
+          // error msg
+        } else {
+          $this.append($clone)
+          $clone.draggable({
+            containment: 'parent',
+          })
+          $('.board__income-current').html(incomeValue - billValue)
+        }
+      }
     }
   })
 }
--- resources/js/modules/moneymaker.js
+++ resources/js/modules/moneymaker.js
@@ -2,13 +2,27 @@
 import $ from 'jquery'
 
 function moneyMaker() {
-  const bills = ['../images/50_kroner.jpg','../images/100_kroner.jpg', '../images/200_kroner.jpg', '../images/500_kroner.jpg', '../images/1000_kroner.jpg']
+  const bills = [
+    '../images/50_kroner.jpg',
+    '../images/100_kroner.jpg',
+    '../images/200_kroner.jpg',
+    '../images/500_kroner.jpg',
+    '../images/1000_kroner.jpg'
+  ]
+
   const $output = $('.board__sidebar-bills')
   const billValues = [50, 100, 200, 500, 1000]
   let count = 0
+
   for (let bill in bills) {
-    $output.append(`<img class="bill" data-bill="${billValues[count]}" src="${bills[bill]}"/>`)
     count++
+    $output.append(
+      `<img
+        class="bill"
+        data-bill="${billValues[count - 1]}"
+        src="${bills[bill]}" />
+      `
+    )
   }
   return bills
 }
