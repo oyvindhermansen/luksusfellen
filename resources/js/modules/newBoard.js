3228c98feafb9d5d59a7a2f969c2898966f3c597
oyvindhermansen <oyvind@ludensreklame.no>
2016-04-24 22:32:56 +0200

    made it possible to add a new table. Error handling is also working

--- resources/js/modules/newBoard.js
+++ resources/js/modules/newBoard.js
@@ -4,8 +4,38 @@ import { dropit } from './drop'
 
 function newBoard($trigger, $appendTo) {
   $trigger.on('click', () => {
-    $appendTo.append(`<li><div class="board__section"></div></li>`)
-    dropit()
+    const $overlay = $('.overlay')
+    const $overlayBtn = $('.overlay__button')
+    const $close = $('.overlay__close')
+
+    $overlay.fadeIn(200)
+
+    $overlayBtn.on('click', () => {
+      const $nameInput = $('.overlay__name-input')
+      const $overlay = $('.overlay')
+      const $errorMsg = $('.overlay__error')
+
+      if ($nameInput.val() === '') {
+        $errorMsg.html('<p>Du må skrive inn et navn.</p>')
+        return false
+      } else {
+        $errorMsg.html('')
+        $nameInput.focus()
+        $overlay.fadeOut(200)
+        $appendTo.append(`<li><div class="board__section"><div class="board__section-name">${$nameInput.val()}</div></div></li>`)
+        $nameInput.val('')
+        dropit()
+      }
+    })
+    $close.on('click', () => {
+      const $nameInput = $('.overlay__name-input')
+      const $overlay = $('.overlay')
+      const $errorMsg = $('.overlay__error')
+
+      $errorMsg.html('')
+      $nameInput.focus()
+      $overlay.fadeOut(200)
+    })
   })
 }
 
